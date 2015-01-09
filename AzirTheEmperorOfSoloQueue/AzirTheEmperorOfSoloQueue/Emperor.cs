b7d375b3d22197fb0d849ed58f3f149c507eddc0
Scias <scias@freelancer.com>
2015-01-09 21:23:52 +0200

    yh

---
+++ AzirTheEmperorOfSoloQueue/AzirTheEmperorOfSoloQueue/Emperor.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace AzirTheEmperorOfSoloQueue
+{
+    class Emperor
+    {
+        internal static Spell Q, W, E, R;
+        internal static SpellSlot IgniteSlot;
+        internal static Menu Config;
+        internal static Obj_AI_Hero Player;
+        internal static List<Spell> Spells = new List<Spell>();
+        internal static Orbwalking.Orbwalker Orb;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        internal static void Game_OnGameLoad(EventArgs args)
+        {
+            Config = new Menu("Azir - SoloQ God","azir",true);
+            var ts = new Menu("Target Selector", "ts");
+            TargetSelector.AddToMenu(ts);
+            Config.AddSubMenu(ts);
+
+            var orbz = new Menu("Orbwalker","orb");
+            Orb = new Orbwalking.Orbwalker(orbz);
+            Config.AddSubMenu(orbz);
+
+            Config.AddItem(new MenuItem("trainMode", "Chu chuuu!!!").SetValue(new KeyBind('Z', KeyBindType.Press)));
+
+            Config.AddToMainMenu();
+            Q = new Spell(SpellSlot.Q, 1250);
+            W = new Spell(SpellSlot.W, 450);
+            E = new Spell(SpellSlot.E, 1250);
+            R = new Spell(SpellSlot.R, 500);
+
+            Q.SetSkillshot(0.25f, 100, 500, false, SkillshotType.SkillshotLine);
+            W.SetSkillshot(0f, 425, float.MaxValue, false, SkillshotType.SkillshotCircle);
+            R.SetSkillshot(0.2f, 700, 1300, false, SkillshotType.SkillshotLine);
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            GameObject.OnCreate += VectorManager.GameObject_OnCreate;
+            GameObject.OnDelete += VectorManager.GameObject_OnDelete;
+        }
+        
+        internal static void Game_OnGameUpdate(EventArgs args)
+        {
+            if (Player.InFountain() || Player.IsRecalling()) return;
+
+            EscapeMode();
+            FightMode();
+        }
+
+        internal static void FightMode()
+        {
+            var target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
+            if (target == null) return;
+            if (VectorManager.AzirObject == null && W.IsReady())
+            {
+                W.Cast(VectorManager.NormalizePosition(target.Position.To2D()), true);
+                if (VectorManager.IsWithinSoldierRange(target))
+                {
+                    Orbwalking.ResetAutoAttackTimer();
+                    Player.IssueOrder(GameObjectOrder.AttackUnit, target);
+                }
+            }
+
+            if (VectorManager.IsWithinSoldierRange(target))
+            {
+                Orbwalking.ResetAutoAttackTimer();
+                Player.IssueOrder(GameObjectOrder.AttackUnit, target);
+            }
+
+            if (VectorManager.AzirObject != null && Q.IsReady() && target.IsValidTarget(Q.Range))
+            {
+                Q.Cast(target, true);
+            }
+            if (VectorManager.AzirObject != null && E.IsReady() && target.IsValidTarget(E.Range))
+            {
+                // need to check if player is behind AzirObject
+                if (VectorManager.IsInFront(VectorManager.AzirObject.Position, target.Position))
+                {
+                    E.Cast(VectorManager.AzirObject.Position, true);
+                }
+            }
+
+        }
+
+        internal static void EscapeMode()
+        {
+            if (!Config.Item("trainMode").GetValue<KeyBind>().Active) return;
+
+            if (!W.IsReady() || !Q.IsReady() || !E.IsReady()) return;
+            W.Cast(VectorManager.NormalizePosition(Game.CursorPos.To2D()), true);
+            E.Cast(VectorManager.NormalizePosition(Game.CursorPos.To2D()), true);
+            Q.Cast(Game.CursorPos, true);
+        }
+    }
+}
---
+++ AzirTheEmperorOfSoloQueue/AzirTheEmperorOfSoloQueue/VectorManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace AzirTheEmperorOfSoloQueue
+{
+    class VectorManager
+    {
+        internal static GameObject AzirObject;
+        internal static void GameObject_OnCreate(GameObject obj, EventArgs args)
+        {
+            if (obj.Name == "AzirSoldier")
+            {
+                AzirObject = obj;
+            }
+        }
+
+        internal static void GameObject_OnDelete(GameObject sender, EventArgs args)
+        {
+            if (sender.Name == "AzirSoldier")
+            {
+                AzirObject = null;
+            }
+        }
+
+        internal static Vector2 NormalizePosition(Vector2 position)
+        {
+            var pos = ObjectManager.Player.Position.To2D();
+            return pos.Distance(position) > 450 ? (position - pos).Normalized() * 450 : position;
+        }
+
+        internal static bool IsWithinSoldierRange(Obj_AI_Base unit)
+        {
+            return unit.Distance(AzirObject.Position) <= 450;
+        }
+
+        internal static bool IsInFront(Vector3 unit, Vector3 direction)
+        {
+            double infront = (ObjectManager.Player.Position.X - unit.X)*direction.X
+                             + (ObjectManager.Player.Position.Y - unit.Y)*direction.Y
+                             + (ObjectManager.Player.Position.Z - unit.Z)*direction.Z;
+            return infront > 0.0;
+        }
+    }
+}
